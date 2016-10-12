6d2aaa3298c733f5169066fb7d1f4d5fc0ff9c0f
Arita Yuta <k104009y@mail.kyutech.jp>
2016-10-13 01:00:36 +0900

    [WIP] modify main callback func

--- target_obejct_detector/src/target_object_recognizer.cpp
+++ target_obejct_detector/src/target_object_recognizer.cpp
@@ -47,9 +47,41 @@ TargetObjectRecognizer::~TargetObjectRecognizer()
 void TargetObjectRecognizer::detectedCallback(const jsk_recognition_msgs::BoundingBoxArray::ConstPtr &detected_objects)
 {
   for (size_t i = 0; i < detected_objects->boxes.size(); ++i) {
-    target_object_candidates_.push_back(detected_objects->boxes[i].pose);
+    bool is_stored(false);
+    geometry_msgs::PoseStamped in_pose;
+    in_pose.pose = detected_objects->boxes[i].pose;
+    in_pose.header = detected_objects->boxes[i].header;
+    geometry_msgs::PoseStamped out_pose;
+    try {
+      tf_.transformPose("map", in_pose, out_pose);
+    } catch (tf::TransformException &ex) {
+      ROS_ERROR("%s",ex.what());
+    }
+    for (size_t j = 0; j < target_object_candidates_.size(); ++j) {
+      double dist = this->calcDistance(target_object_candidates_[j].getPose(),
+                                       out_pose.pose);
+      if (dist < 1.0) {
+        target_object_candidates_[j].addCounter();
+        target_object_candidates_[j].addTargetObject(out_pose,
+                                                     detected_objects->boxes[i]);
+        is_stored = true;
+      }
+    }
+    if (! is_stored) {
+      target_object_candidates_.push_back(TargetObject(out_pose,
+                                                       detected_objects->boxes[i]));
+    }
   }
-
+  ROS_INFO_STREAM("target object candidates : " << target_object_candidates_.size());
+  jsk_recognition_msgs::BoundingBoxArray box_array;
+  for (size_t i = 0; i < target_object_candidates_.size(); ++i) {
+    if(target_object_candidates_[i].getCounter() > 5){
+      box_array.boxes.push_back(target_object_candidates_[i].getBox());
+    }
+  }
+  box_array.header.stamp = ros::Time::now();
+  box_array.header.frame_id = "map";
+  recognized_pub_.publish(box_array);
 }
 
 void TargetObjectRecognizer::run()
