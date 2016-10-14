1b645a8d9804db8c7de7d1060dff16848de83c14
Arita Yuta <k104009y@mail.kyutech.jp>
2016-10-14 20:02:57 +0900

    modify ロボットの位置から遠くて検出回数が少ない候補は消す処理を追加

--- target_obejct_detector/src/target_object_recognizer.cpp
+++ target_obejct_detector/src/target_object_recognizer.cpp
@@ -67,6 +67,7 @@ TargetObjectRecognizer::TargetObjectRecognizer(ros::NodeHandle nh)
 {
   detected_sub_ = nh_.subscribe("/clustering_result", 1, &TargetObjectRecognizer::detectedCallback, this);
   recognized_pub_ = nh_.advertise<jsk_recognition_msgs::BoundingBoxArray>("/recognized_result", 1);
+  robotpose_sub_ = nh_.subscribe("/amcl_pose", 1, &TargetObjectRecognizer::robotPoseCallback, this);
 }
 
 TargetObjectRecognizer::~TargetObjectRecognizer()
@@ -75,42 +76,65 @@ TargetObjectRecognizer::~TargetObjectRecognizer()
 
 void TargetObjectRecognizer::detectedCallback(const jsk_recognition_msgs::BoundingBoxArray::ConstPtr &detected_objects)
 {
-  for (size_t i = 0; i < detected_objects->boxes.size(); ++i) {
-    bool is_stored(false);
-    geometry_msgs::PoseStamped in_pose;
-    in_pose.pose = detected_objects->boxes[i].pose;
-    in_pose.header = detected_objects->boxes[i].header;
-    geometry_msgs::PoseStamped out_pose;
-    try {
-      tf_.transformPose("map", in_pose, out_pose);
-    } catch (tf::TransformException &ex) {
-      ROS_ERROR("%s",ex.what());
-    }
-    for (size_t j = 0; j < target_object_candidates_.size(); ++j) {
-      double dist = this->calcDistance(target_object_candidates_[j].getPose(),
-                                       out_pose.pose);
-      if (dist < 1.0) {
-        target_object_candidates_[j].addCounter();
-        target_object_candidates_[j].addTargetObject(out_pose,
-                                                     detected_objects->boxes[i]);
-        is_stored = true;
+  {
+    boost::mutex::scoped_lock(robot_pose_mutex_);
+    for (size_t i = 0; i < detected_objects->boxes.size(); ++i) {
+      bool is_stored(false);
+      geometry_msgs::PoseStamped in_pose;
+      in_pose.pose = detected_objects->boxes[i].pose;
+      in_pose.header = detected_objects->boxes[i].header;
+      geometry_msgs::PoseStamped out_pose;
+      try {
+        tf_.transformPose("map", in_pose, out_pose);
+      } catch (tf::TransformException &ex) {
+        ROS_ERROR("%s",ex.what());
+      }
+      for (size_t j = 0; j < target_object_candidates_.size(); ++j) {
+        double dist = this->calcDistance(target_object_candidates_[j].getPose(),
+                                         out_pose.pose);
+        if (dist < 1.0) {
+          target_object_candidates_[j].addCounter();
+          target_object_candidates_[j].addTargetObject(out_pose,
+                                                       detected_objects->boxes[i]);
+          is_stored = true;
+        }
+      }
+      if (! is_stored) {
+        target_object_candidates_.push_back(TargetObject(out_pose,
+                                                         detected_objects->boxes[i]));
       }
     }
-    if (! is_stored) {
-      target_object_candidates_.push_back(TargetObject(out_pose,
-                                                       detected_objects->boxes[i]));
+    jsk_recognition_msgs::BoundingBoxArray box_array;
+    for (size_t i = 0; i < target_object_candidates_.size(); ++i) {
+      if(target_object_candidates_[i].getCounter() > 5){
+        box_array.boxes.push_back(target_object_candidates_[i].getBox());
+      }
+    }
+    ROS_INFO_STREAM("target object candidates : " << target_object_candidates_.size());
+    box_array.header.stamp = ros::Time::now();
+    box_array.header.frame_id = "map";
+    recognized_pub_.publish(box_array);
+  }
+}
+
+void TargetObjectRecognizer::robotPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& amcl_pose)
+{
+  //robotの位置をサブスクライブして、バッファにいれる（要mutex）
+  {
+    boost::mutex::scoped_lock(robot_pose_mutex_);
+    latest_robot_pose_ = *amcl_pose;
+    // 最新のロボットの位置と比較してn[m]離れててかつcounterがしきい値以下ならその候補は消す
+    for (std::vector<TargetObject>::iterator it = target_object_candidates_.begin(); it != target_object_candidates_.end(); ) {
+      double dist_from_robot = this->calcDistance(it->getPose(), latest_robot_pose_.pose.pose);
+      if(dist_from_robot >= 5.0 && it->getCounter() < 3)
+      {
+        it = target_object_candidates_.erase(it);
+        continue;
+      }
+      it++;
     }
   }
-  ROS_INFO_STREAM("target object candidates : " << target_object_candidates_.size());
-  jsk_recognition_msgs::BoundingBoxArray box_array;
-  for (size_t i = 0; i < target_object_candidates_.size(); ++i) {
-    if(target_object_candidates_[i].getCounter() > 5){
-      box_array.boxes.push_back(target_object_candidates_[i].getBox());
-    }
-  }
-  box_array.header.stamp = ros::Time::now();
-  box_array.header.frame_id = "map";
-  recognized_pub_.publish(box_array);
+
 }
 
 void TargetObjectRecognizer::run()
