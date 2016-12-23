08f77fc7ba00ad65cfd91f7a2628c02336bc6e02
MoriKen254 <masamorobo@gmail.com>
2016-12-23 16:54:44 +0900

    Add install for apt utilities in before script.

--- before_script.sh
+++ before_script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+apt-get install -y software-properties-common python-software-properties
 yes | apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; apt-get update -qq; apt-get install -y libpcl-all;
